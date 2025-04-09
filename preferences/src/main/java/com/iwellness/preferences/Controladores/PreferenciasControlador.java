6483d5917b315e3aa782dee2b8c7914f34fdd6c5
clarck2547 <63880026+clarck2547@users.noreply.github.com>
2025-04-09 12:38:46 -0500

    cross origin

--- preferences/src/main/java/com/iwellness/preferences/Controladores/PreferenciasControlador.java
+++ preferences/src/main/java/com/iwellness/preferences/Controladores/PreferenciasControlador.java
@@ -3,6 +3,7 @@ package com.iwellness.preferences.Controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.iwellness.preferences.Entidades.Preferencias;
 import com.iwellness.preferences.Servicios.PreferenciasServicio.IPreferenciasServicio;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/preferencias")
 public class PreferenciasControlador {
