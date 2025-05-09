3bf2d19da21e165ea95950ff378dd065a7b36f96
Sara Peña <sarapenagomez02@gmail.com>
2025-05-08 21:53:45 -0500

    Cola

--- preferences/src/main/java/com/iwellness/preferences/Servicios/Rabbit/MensajeServicePreferencesConfig.java
+++ preferences/src/main/java/com/iwellness/preferences/Servicios/Rabbit/MensajeServicePreferencesConfig.java
@@ -1,6 +1,5 @@
 package com.iwellness.preferences.Servicios.Rabbit;
 
-
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,14 +7,19 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Configuration
 public class MensajeServicePreferencesConfig {
+
     public static final String EXCHANGE_NAME = "message_exchange";
+
     public static final String QUEUE_NAME_PREFERENCES = "my_queue_preferences";
     public static final String ROUTING_KEY_PREFERENCE = "my_routing_key_preference";
+
     public static final String QUEUE_NAME_SERVICIOXPREFERENCIA = "my_queue_servicexpreferences";
     public static final String ROUTING_KEY_SERVICIOXPREFERENCIA = "my_routing_key_servicexpreferences";
+
     public static final String QUEUE_NAME_TURISTAXPREFERENCIA = "my_queue_turistxpreferences";
     public static final String ROUTING_KEY_TURISTAXPREFERENCIA = "my_routing_key_turistxpreferences";
 
@@ -35,7 +39,7 @@ public class MensajeServicePreferencesConfig {
         template.setMessageConverter(messageConverter());
         return template;
     }
-    
+
     @Bean
     public Queue queuePreferences() {
         return new Queue(QUEUE_NAME_PREFERENCES, true);
@@ -50,21 +54,28 @@ public class MensajeServicePreferencesConfig {
     public Queue queueTuristxPreferences() {
         return new Queue(QUEUE_NAME_TURISTAXPREFERENCIA, true);
     }
-    
 
     @Bean
-    public Binding bindingPreferences(Queue queue, TopicExchange exchange) {
+    public Binding bindingPreferences(
+        @Qualifier("queuePreferences") Queue queue,
+        TopicExchange exchange
+    ) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_PREFERENCE);
     }
 
     @Bean
-    public Binding bindingServicexPreferences(Queue queue, TopicExchange exchange) {
+    public Binding bindingServicexPreferences(
+        @Qualifier("queueServicexPreferences") Queue queue,
+        TopicExchange exchange
+    ) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_SERVICIOXPREFERENCIA);
     }
 
     @Bean
-    public Binding bindingTuristxPreferences(Queue queue, TopicExchange exchange) {
+    public Binding bindingTuristxPreferences(
+        @Qualifier("queueTuristxPreferences") Queue queue,
+        TopicExchange exchange
+    ) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_TURISTAXPREFERENCIA);
     }
-    
 }
